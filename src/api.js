71197da7ac501302eea7374442d41f24eacaf800
Syujaaa <syujafarras@gmail.com>
2025-11-12 22:59:51 +0700

    Add form

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://edukasiJantung.42web.io/pusatdokumen/api/edukasi.php", 
+  baseURL: "https://edukasiJantung.42web.io/api/edukasi.php", 
 });
 
 export default api;
