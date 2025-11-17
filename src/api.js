ddd66caf489d140e511b61350bc4001d32d9ed36
Syujaaa <syujafarras@gmail.com>
2025-11-17 19:44:46 +0700

    Add print

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://edukasijantungapi.syujaaa.deno.net", 
-  baseURL: "http://localhost:3000", 
+  baseURL: "https://edukasijantungapi.syujaaa.deno.net", 
+  // baseURL: "http://localhost:3000", 
 });
 
 api.interceptors.request.use(
