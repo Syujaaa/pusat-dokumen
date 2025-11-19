eed3265aaac098e62ce4e61c0438a037cca2b182
Syujaaa <syujafarras@gmail.com>
2025-11-19 11:55:44 +0700

    Update UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-[#F5F9FF] border-t border-blue-200 mt-8 py-4 no-print">
       <div className="max-w-7xl mx-auto px-4 text-center text-gray-600 text-sm flex flex-col items-center gap-1">
-        <p className="flex items-center gap-1 justify-center">
+        {/* <p className="flex items-center gap-1 justify-center">
           <a
             href="https://farrassyuja.my.id/"
             target="_BLANK"
@@ -13,7 +13,7 @@ export default function Footer() {
           >
             Farras Syuja
           </a>
-        </p>
+        </p> */}
 
         <p className="text-gray-700">
           © {new Date().getFullYear()} Edukasi Gagal Jantung. All rights
